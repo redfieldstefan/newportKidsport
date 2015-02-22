db9fd10f9557419eb02ebd3eb1cda95dd42d210d
Stefan Redfield <redfield.stefan@gmail.com>
2015-02-22 15:38:00 -0800

    updates mobile responsiveness

--- js/effects.js
+++ js/effects.js
@@ -5,4 +5,18 @@ $(document).ready(function() {
 	$('.main img').attr('src', thisImg);
 })
 
+$('nav ul li:nth-child(2)').click(function(){
+	
+	navOpen = false;
+	
+	if (!navOpen) {
+		$('nav ul li ul ').css('display', 'block');
+		navOpen = true;
+	} else {
+		$('nav ul li ul ').css('display', 'none');
+		navOpen = false;
+	}
+})
+
+
 })
