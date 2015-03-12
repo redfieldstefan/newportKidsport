6701bba0a1ad191fc4248fa8bea9000cd6f73da6
Stefan Redfield <redfield.stefan@gmail.com>
2015-03-11 18:23:03 -0700

    update fabrics page with messaging and images

--- js/effects.js
+++ js/effects.js
@@ -3,7 +3,13 @@ $(document).ready(function() {
 	$('.small-feature img').mouseenter(function(){
 	var thisImg = this.src;
 	$('.main img').attr('src', thisImg);
-})
+	})
+
+	$('.swatch-options img').mouseenter(function(){
+	var thisImg = this.src;
+	var thisParent = $(this).parents().children('.swatch img');
+	thisParent.attr('src', thisImg);
+	})
 
 $('nav ul li:nth-child(2)').click(function(){
 	
