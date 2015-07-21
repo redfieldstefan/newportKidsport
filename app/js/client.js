a4abf2e5cabb5655c6e1322dd6b7f5b26a295540
Stefan Redfield <redfield.stefan@gmail.com>
2015-07-20 18:46:55 -0700

    first commit angular setup

---
+++ app/js/client.js
@@ -0,0 +1,54 @@
+'use strict';
+
+require('angular/angular');
+require('angular-route');
+require('angular-sanitize');
+require('angular-animate');
+
+var kidsportApp = angular.module('kidsportApp', ['ngRoute', 'ngSanitize', 'ngAnimate']);
+
+//controllers
+require('./controllers/main_controller')(kidsportApp);
+
+//directives
+require('./directives/nav_directive')(kidsportApp);
+
+//routes
+
+kidsportApp.config(['$routeProvider', function($routeProvider){
+  $routeProvider
+    .when('/', {
+      templateUrl: 'views/landing.html',
+      controller: 'MainController'
+    })
+    .when('/about', {
+      templateUrl: 'views/about.html',
+      controller: 'MainController'
+    })
+    .when('/contact', {
+      templateUrl: 'views/contact.html',
+      controller: 'MainController'
+    })
+    .when('/fabrics', {
+      templateUrl: 'views/fabrics.html',
+      controller: 'MainController'
+    })
+    .when('/stockist', {
+      templateUrl: 'views/stockist.html',
+      controller: 'MainController'
+    })
+    .when('/apparel/raincape', {
+      templateUrl: 'views/apparel/raincape.html',
+      controller: 'ApparelController'
+    })
+    .when('/apparel/babycape', {
+      templateUrl: 'views/apparel/babycape.html',
+      controller: 'ApparelController'
+    })
+    .when('/apparel/rainbubble', {
+      templateUrl: 'views/apparel/rainbubble.html',
+      controller: 'ApparelController'
+    })
+    .otherwise({redirectTo:'/'});
+}]);
+
